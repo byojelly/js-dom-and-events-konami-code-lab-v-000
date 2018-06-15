a40348cd9249cd40c1e96dc2886ba06914b5102d
Alex <github email address>
2018-06-14 21:55:50 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,17 +1,18 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
   let index = 0;
-
+  let indexArray=[]
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.which || e.detail);
-
+debugger
     if (code[index] === key) {
+      indexArray.push(key)
       index++;
 
-      if (index === code.length) {
-        alert('Hurray!');
-
+      if (index === (code.length - 1)) {
+        return alert("Hurray!");
+debugger
         index = 0;
       }
     } else {
@@ -19,13 +20,3 @@ function init() {
     }
   });
 }
-
-//this.addEventListener('keydown', function(e){console.log(e.which)})
-
-
-// up arrow = 38
-// down arrow = 40
-// l arrow = 37
-// r arrow = 39
-// a = 65
-// b = 66
